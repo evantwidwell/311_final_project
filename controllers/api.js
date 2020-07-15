887f086eb81276d82ff5b0c10e032abef770105d
evantwidwell <evanski@gmail.com>
2020-07-14 20:51:41 -0500

    bug fixes

--- controllers/api.js
+++ controllers/api.js
@@ -2,16 +2,10 @@ const mysql = require('mysql')
 const pool = require('../sql/connection')
 const { handleSQLError } = require('../sql/error')
 
-const getAllCustomers = (req, res) => {
-  // SELECT ALL USERS
-  pool.query("SELECT first_name, last_name, email_address, phone FROM customers", (err, rows) => {
-    if (err) return handleSQLError(res, err)
-    return res.json(rows);
-  })
-}
+
 const createCustomer = (req, res) => {
   let sql = "INSERT INTO customers (?? , ??, ??, ??) values(?, ?, ?, ?)"
-  console.log(req.body)
+  // console.log(req.body)
   sql = mysql.format(sql, ['first_name','last_name','email_address','phone',req.body.firstName, req.body.lastName, req.body.emailAddress, req.body.phone])
 
   pool.query(sql, (err, results) => {
@@ -19,6 +13,13 @@ const createCustomer = (req, res) => {
     return res.json({ newId: results.insertId });
   })
 }
-
+const getAllCustomers = (req, res) => {
+  // SELECT ALL USERS
+  pool.query("SELECT * FROM customers", (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    
+    return res.json(rows);
+  })
+}
 module.exports = { getAllCustomers, createCustomer
 }
